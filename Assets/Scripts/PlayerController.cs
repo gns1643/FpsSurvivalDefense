2966850b26f23c71ec56a2f0d94a47a3e2201979
gns1643 <gns1643@naver.com>
2025-06-26 17:29:00 +0900

    Part2. 심화 캐릭터 움직임
    점프시 앉기 해제,
    달리기시 앉기 해제가 안됨
    수정 필요

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,48 @@
+using System.Collections;
+using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
 
 public class PlayerController : MonoBehaviour
 {
+    //속도 변수
     [SerializeField]
     private float walkSpeed;
 
     [SerializeField]
+    private float runSpeed;
+    [SerializeField]
+    private float crouchSpeed;
+
+    private float applySpeed;
+
+    [SerializeField]
+    private float jumpForce;
+
+    //상태변수
+    private bool isRun = false;
+    private bool isGround = false;
+    private bool isCrouch = false;
+
+    //얼마나 앉을지 변수
+    [SerializeField]
+    private float crouchPosY;
+    private float originPosY;
+    private float applyCrouchPosY;
+
+    //땅착지여부 파악
+    private CapsuleCollider capsuleCollider;
+
+    //민감도
+    [SerializeField]
     private float lookSensitivity;
 
+    //카메라 변수
     [SerializeField]
     private float cameraRotationLimit;
     private float currentCameraRotationX = 0;
 
+    //필요한 컴포넌트들
     [SerializeField]
     private Camera theCamera;
 
@@ -22,15 +52,113 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         myRigid = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        applySpeed = walkSpeed;
+        originPosY = theCamera.transform.localPosition.y;
+        applyCrouchPosY = originPosY;
     }
 
     void Update()
     {
+        IsGround();
+        TryJump();
+        TryRun();
+        TryCrouch();
         Move();
         CameraRotation();
         CharacterRotation();
     }
 
+    void TryCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+
+    void Crouch()
+    {
+        isCrouch = !isCrouch;
+        if (isCrouch)
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        }
+        else
+        {
+            applySpeed = walkSpeed;
+            applyCrouchPosY = originPosY;
+        }
+        //theCamera.transform.localPosition = new Vector3(theCamera.transform.localPosition.x, applyCrouchPosY, theCamera.transform.localPosition.z);
+        StartCoroutine(CrouchCoroutine());
+    }
+
+    IEnumerator CrouchCoroutine()
+    {
+        float _posY = theCamera.transform.localPosition.y;
+        int count = 0;
+        while(_posY != applyCrouchPosY)
+        {
+            count++;
+            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.1f);
+            theCamera.transform.localPosition = new Vector3(0, _posY, 0);
+            if(count > 15)
+            {
+                break;
+            }
+            yield return null;
+        }
+        theCamera.transform.localPosition = new Vector3(0, applyCrouchPosY, 0);
+
+    }
+
+    void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+
+    void TryJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        if (isCrouch)
+            Crouch();
+        
+        myRigid.linearVelocity = transform.up * jumpForce;
+    }
+
+    void TryRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+    void Running()
+    {
+        if (isCrouch)
+            Crouch();
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+
+    void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
+    }
     void Move()
     {
         float _moveDirX = Input.GetAxisRaw("Horizontal");
@@ -39,7 +167,7 @@ public class PlayerController : MonoBehaviour
         Vector3 _moveHorizontal = transform.right * _moveDirX;
         Vector3 _moveVertical = transform.forward * _moveDirZ;
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
 
         myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
     }
