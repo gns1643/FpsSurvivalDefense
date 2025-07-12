6e0d8834f7e6bda8bced7e35bc04d1375a3ee5a8
gns1643 <gns1643@naver.com>
2025-07-12 21:24:24 +0900

    Part26. 건축불가

--- Assets/Scripts/UI_Scripts/CraftManual.cs
+++ Assets/Scripts/UI_Scripts/CraftManual.cs
@@ -68,7 +68,7 @@ public class CraftManual : MonoBehaviour
 
     void Build()
     {
-        if (isPreviewActivated)
+        if (isPreviewActivated && go_Preview.GetComponent<PreviewObject>().IsBuildable())
         {
             Instantiate(go_Prefab, hitInfo.point, Quaternion.identity);
             Destroy(go_Preview);
