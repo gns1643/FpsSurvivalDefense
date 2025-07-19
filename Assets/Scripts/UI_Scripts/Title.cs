0d981503644d53b5904b41864ff163508c99c906
gns1643 <gns1643@naver.com>
2025-07-19 15:05:36 +0900

    Part33. 타이틀메뉴

---
+++ Assets/Scripts/UI_Scripts/Title.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public string sceneName;
+    public void ClickStart()
+    {
+        Debug.Log("로딩");
+        SceneManager.LoadScene(sceneName);
+    }
+    public void ClickLoad()
+    {
+        Debug.Log("로드");
+    }
+    public void ClickExit()
+    {
+        Debug.Log("게임종료");
+        Application.Quit();
+    }
+}
+
